fcbfd2124eed6f0e5ae1036f98d3ef2cfca15bb9
mrbunny88 <72190637+mrbunny88@users.noreply.github.com>
2025-02-21 14:23:26 +0530

    Add files via upload
    commit by naitik : unit 2

---
+++ ExplcitJava/app/src/main/java/com/vairagicodes/explicitintent/TargatedActivity.java
@@ -0,0 +1,37 @@
+package com.vairagicodes.explicitintent;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TargatedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_targated);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        String username = getIntent().getStringExtra("User Name");
+        String email = getIntent().getStringExtra("User Email");
+
+        TextView textView = findViewById(R.id.username_tv);
+        TextView textView1 = findViewById(R.id.user_email_tv);
+
+        textView.setText(username);
+        textView1.setText(email);
+
+
+    }
+
+    }
